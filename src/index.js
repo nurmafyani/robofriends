6edc6b7721af0c4535ead04c002cd523a32924ff
Nurmalia Afiyani <nurmalia.afiyani@gmail.com>
2020-08-14 06:13:43 +0700

    Adding file react in git

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Card from './Card';
+import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+
+ReactDOM.render(
+	<div>
+		<Card />
+		<Card />
+		<Card />
+		<Card />
+		<Card />
+	</div>
+	, document.getElementById('root'));
+serviceWorker.unregister();
---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = {
+	{
+		id: 1,
+		name: 'Nurmalia Afiyani',
+		username: 'Nurma',
+		email: 'nurmalia.afiyani@gmail.ocm'
+	}
+	{
+		id: 2,
+		name: 'Nurmalia Afiyani',
+		username: 'Nurma',
+		email: 'nurmalia.afiyani@gmail.ocm'
+	}
+	{
+		id: 3,
+		name: 'Nurmalia Afiyani',
+		username: 'Nurma',
+		email: 'nurmalia.afiyani@gmail.ocm'
+	}
+	{
+		id: 4,
+		name: 'Nurmalia Afiyani',
+		username: 'Nurma',
+		email: 'nurmalia.afiyani@gmail.ocm'
+	}
+	{
+		id: 5,
+		name: 'Nurmalia Afiyani',
+		username: 'Nurma',
+		email: 'nurmalia.afiyani@gmail.ocm'
+	}
+}
